e26ffb19e2bb7126400f5d0a35a8bfc271296dbd
zhangge1998 <1263061442@qq.com>
2017-07-21 16:44:24 +0800

    add function buildFinalTotal

--- spec/main-test.js
+++ spec/main-test.js
@@ -99,4 +99,74 @@ describe('unit test', ()=> {
       expect(buildNowTotal(itemTotal)).toEqual(nowTotal);
     })
   })
+  describe('buildFinalTotal',()=>{
+    let nowTotal={
+      list:[{
+        item: {
+          id: 'ITEM0001',
+          name: '黄焖鸡',
+          price: 18.00
+        },
+        count: 1,
+        subTotal: 18.00
+      },
+        {
+          item:{
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+          },
+          count:2,
+          subTotal:12.00
+        },
+        {
+          item:{
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+          },
+          count:1,
+          subTotal:8.00
+        }],
+      nowTotal:38.00
+    }
+    it('should return right finalTotal',()=>{
+      const finalTotal={
+        nowTotal:{
+          list:[{
+            item: {
+              id: 'ITEM0001',
+              name: '黄焖鸡',
+              price: 18.00
+            },
+            count: 1,
+            subTotal: 18.00
+          },
+            {
+              item:{
+                id: 'ITEM0013',
+                name: '肉夹馍',
+                price: 6.00
+              },
+              count:2,
+              subTotal:12.00
+            },
+            {
+              item:{
+                id: 'ITEM0022',
+                name: '凉皮',
+                price: 8.00
+              },
+              count:1,
+              subTotal:8.00
+            }],
+          nowTotal:38.00,
+          type:'指定菜品半价'
+        },
+        save:13,
+        finalTotal:25
+      }
+      expect(buildFinalTotal(nowTotal)).toEqual(finalTotal);
+    })
+  })
 })
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,4 @@
+let save=[];
 let buildItemTotal=(selectedItems)=> {
   let allItems = loadAllItems();
   let itemTotal = [];
@@ -20,7 +21,7 @@ let isExit=(allItems, selectedItems)=> {
       return 1;
     }
   }
-}
+};
 
 let buildNowTotal=(itemTotal)=> {
   let total = 0;
@@ -28,6 +29,52 @@ let buildNowTotal=(itemTotal)=> {
     total += itemTotal1.subTotal;
   }
   return {list: itemTotal, nowTotal: total};
+};
+
+let buildFinalTotal=(nowTotal)=> {
+  let save = sSave(nowTotal);
+  let finalTotal = nowTotal.nowTotal - save;
+  return {nowTotal: nowTotal, save: save, finalTotal: finalTotal};
+}
+
+let sSave=(nowTotal)=> {
+  let promotions = loadPromotions();
+  if (nowTotal.finalTotal >= 30) {
+    let save1 = 6;
+    let save2 = 0;
+    for (let now of nowTotal.list) {
+      if (isHalfPrice(now.item, promotions)) {
+        save2 += now.subTotal / 2;
+      }
+    }
+    if (save1 > save2) {
+      nowTotal.type = "满30减6元，省6元";
+      return save1;
+    }
+    else {
+      nowTotal.type = "指定菜品半价";
+      return save2;
+    }
+  }
+  else {
+    let save3 = 0;
+    for (let now of nowTotal.list) {
+      if (isHalfPrice(now.item, promotions)) {
+        save3 += now.subTotal / 2;
+        nowTotal.type = "指定菜品半价";
+      }
+    }
+    return save3;
+  }
+}
+
+let isHalfPrice=(nowTotal, promotions)=> {
+  for (let item of promotions[1].items) {
+    if (item === nowTotal.id) {
+      save.push(nowTotal.name);
+      return 1;
+    }
+  }
 }
 
 

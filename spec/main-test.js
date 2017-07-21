cc3b2eaffa14b6ae90f3a8cd8bbc2724240c5ae4
zhangge1998 <1263061442@qq.com>
2017-07-21 15:57:45 +0800

    add function buildItemTotal

---
+++ spec/main-test.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ubuntu on 17-7-21.
+ */
+describe('unit test', ()=> {
+  describe('buildItemTotal', ()=> {
+    let inputs = ["ITEM0001 x 1",
+      "ITEM0013 x 2",
+      "ITEM0022 x 1"];
+    it('should return right itemTotal', ()=> {
+      const itemTotal = [{
+        item: {
+          id: 'ITEM0001',
+          name: '黄焖鸡',
+          price: 18.00
+        },
+        count: 1,
+        subTotal: 18.00
+      },
+        {
+          item:{
+            id: 'ITEM0013',
+            name: '肉夹馍',
+            price: 6.00
+          },
+          count:2,
+          subTotal:12.00
+        },
+        {
+          item:{
+            id: 'ITEM0022',
+            name: '凉皮',
+            price: 8.00
+          },
+          count:1,
+          subTotal:8.00
+        }];
+      expect(buildItemTotal(inputs)).toEqual(itemTotal);
+    })
+  })
+})
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,25 @@
-function bestCharge(selectedItems) {
-  return /*TODO*/;
-}
+let buildItemTotal=(selectedItems)=> {
+  let allItems = loadAllItems();
+  let itemTotal = [];
+  for (let cartItem of allItems) {
+    if (isExit(cartItem, selectedItems)) {
+      itemTotal.push({item: cartItem, count: count1, subTotal: subTotal1});
+    }
+  }
+  return itemTotal;
+};
+
+let isExit=(allItems, selectedItems)=> {
+  for (let selectedItem of selectedItems) {
+    let splitInput = selectedItem.split("x");
+    let id = splitInput[0].trim();
+    let count=splitInput[1].trim();
+    if (id === allItems.id) {
+      count1 = parseInt(count);
+      subTotal1 = allItems.price * count1;
+      return 1;
+    }
+  }
+};
+
+
